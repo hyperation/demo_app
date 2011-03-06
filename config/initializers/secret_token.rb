32660a1a86e18a76e67d414ab154e44c7e995dd1
hyperation <hyperation@hotmail.com>
2011-03-05 14:37:47 -1000

    Demo App Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '49d8f4fad25e88d3ef7de5baf906923623fd090c5e88138c06a74311ff5f625b3eb48a3fbf939797319966bd0288bbb41d2d0829bdaeefc63ea2c2137a709e5f'
